00732e7b00e4f2f86fd12b631331aad97d2efa77
syzygy <syzygy@arsthaneas-MacBook-Air.local>
2019-11-24 17:57:47 +0100

    init

---
+++ resources/views/partials/content.blade.php
@@ -0,0 +1,28 @@
+<div class="col col-md-6 col-lg-4"
+    <article class="teaser" @php post_class() @endphp>
+        <!-- <div class="teaser__image-wrapper">
+            @php the_post_thumbnail() @endphp
+        </div> -->
+        <div class="teaser__text-wrapper">
+            <!-- <span class="teaser__label label label--book">
+                <?php echo get_post_meta($post->ID, 'label', true); ?>
+            </span> -->
+            <h3 class="teaser__title">
+                <a href="{{ get_permalink() }}">{!! get_the_title() !!}</a>
+            </h3>
+            <div class="teaser__details">
+                <p class="teaser__detail">
+                    <?php echo get_post_meta($post->ID, 'date', true); ?>
+                </p>
+                <p class="teaser__detail">
+                    <?php echo get_post_meta($post->ID, 'localization', true); ?>
+                </p>
+            </div>
+            <!-- @include('partials/entry-meta') -->
+            <p class="teaser__description">
+                <?php echo get_the_excerpt($post->ID); ?>
+            </p>
+        </div>
+    </article>
+</div>
+
