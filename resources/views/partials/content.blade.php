53db5c11954c6527e6ef636d82b8ce31d0ecc755
syzygy <syzygy@arsthaneas-MacBook-Air.local>
2019-11-25 00:13:39 +0100

    layout

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -1,20 +1,22 @@
-<div class="col col-md-6 col-lg-4"
+<div class="col-xs-12 col-md-6 col-lg-4">
     <article class="teaser" @php post_class() @endphp>
-        <div class="teaser__text-wrapper">
-            <h3 class="teaser__title">
-                <a href="{{ get_permalink() }}">{!! get_the_title() !!}</a>
-            </h3>
-            <div class="teaser__details">
-                <p class="teaser__detail">
-                    <?php echo get_post_meta($post->ID, 'date', true); ?>
-                </p>
-                <p class="teaser__detail">
-                    <?php echo get_post_meta($post->ID, 'localization', true); ?>
+        <div class="teaser__inner">
+            <div class="teaser__text-wrapper">
+                <h3 class="teaser__title">
+                    <a href="{{ get_permalink() }}">{!! get_the_title() !!}</a>
+                </h3>
+                <div class="teaser__details">
+                    <p class="teaser__detail">
+                        <?php echo get_post_meta($post->ID, 'date', true); ?>
+                    </p>
+                    <p class="teaser__detail">
+                        <?php echo get_post_meta($post->ID, 'localization', true); ?>
+                    </p>
+                </div>
+                <p class="teaser__description">
+                    <?php echo get_the_excerpt($post->ID); ?>
                 </p>
             </div>
-            <p class="teaser__description">
-                <?php echo get_the_excerpt($post->ID); ?>
-            </p>
         </div>
     </article>
 </div>
