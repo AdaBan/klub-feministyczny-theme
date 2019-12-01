073ae93ad682b7514a04ad55e68f6849129b9df5
syzygy <syzygy@arsthaneas-MacBook-Air.local>
2019-12-01 18:30:43 +0100

    adding categories

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -1,13 +1,15 @@
 <article @php post_class() @endphp>
-  <header>
-    <h1 class="entry-title">{!! get_the_title() !!}</h1>
-    @include('partials/entry-meta')
-  </header>
-  <div class="entry-content">
-    @php the_content() @endphp
+  <div class="col-xs-12 col-lg-8">
+    <header>
+        <h1 class="entry-title">{!! get_the_title() !!}</h1>
+    </header>
+    <div class="entry-content">
+      @php the_content() @endphp
+    </div>
   </div>
-  <footer>
-    {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
-  </footer>
-  @php comments_template('/partials/comments.blade.php') @endphp
+  <div class="col-xs-12 col-lg-4 offset-xl-1 col-xl-3">
+    <div class="post__image-wrapper">
+      @php the_post_thumbnail(); @endphp
+    </div>
+  </div>
 </article>
--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -1,7 +1,14 @@
 <div class="col-xs-12 col-md-6 col-lg-4">
-    <article class="teaser" @php post_class() @endphp>
+    <article
+        class="teaser teaser--<?php echo get_the_category($post->ID)[0]->slug; ?>"
+    >
         <div class="teaser__inner">
             <div class="teaser__text-wrapper">
+                <span><?php //$ar = get_the_category($post->ID); echo $ar[0]->slug; 
+                if (get_the_category($post->ID)[0]->slug == 'blablabla') {}
+
+
+                ?></span>
                 <h3 class="teaser__title">
                     <a href="{{ get_permalink() }}">{!! get_the_title() !!}</a>
                 </h3>
